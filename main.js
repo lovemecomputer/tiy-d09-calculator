9ca2168fdd24f96e4b4ae070fab3675ee1fa5051
charli elwonger <celwonger@gmail.com>
2017-03-09 15:04:06 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+/* ternjs ref: https://atom.io/packages/atom-ternjs */
+var buttons = document.querySelectorAll('.btn');
+console.log(buttons);
+
+var numberButtons = document.querySelectorAll('.btn-num');
+console.log(numberButtons);
